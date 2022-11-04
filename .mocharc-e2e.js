5af2d5ca9c089d8868850c62ffc6e01580854e26
Luca Nardelli <lucanardelli91@gmail.com>
2022-11-05 00:00:49 +0100

    First commit

---
+++ .mocharc-e2e.js
@@ -0,0 +1,19 @@
+process.env.NODE_ENV = 'test';
+
+// https://github.com/mochajs/mocha/blob/master/example/config/.mocharc.js
+module.exports = {
+  bail: true,
+  color: true,
+  parallel: false,
+  exit: true,
+  extension: ['ts'],
+  timeout: 10000,
+  // require: '@swc/register',
+  // require: 'ts-node/register',
+  file: [
+    './test/setup.ts',
+  ],
+  spec: [
+    './test/**/*.e2e-spec.ts',
+  ]
+};
---
+++ test/setup.ts
@@ -0,0 +1,13 @@
+import * as chai from 'chai';
+import * as chaiSubset from 'chai-subset';
+import { jestSnapshotPlugin } from 'mocha-chai-jest-snapshot';
+
+chai.use(chaiSubset);
+chai.use(jestSnapshotPlugin);
+
+process.env.NODE_ENV = 'test';
+process.env.APP_ENV = 'test';
+
+before(async () => {
+  // Add global setup here
+});
